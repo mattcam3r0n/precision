0b3a28355ecbb73fe28dd0e79c257867ebba0866
Matt Cameron <cameron.matt@gmail.com>
2017-09-30 00:27:40 -0500

    draggable fixes. drag-handle.

--- client/components/draggable/draggable.directive.js
+++ client/components/draggable/draggable.directive.js
@@ -2,24 +2,29 @@ angular.module('drillApp')
 .directive('draggable', ['$document', function($document) {
   return {
     link: function(scope, element, attr) {
-      var startX = 0, startY = 0, x = 0, y = 0;
+      var startX = 0, 
+          startY = 0;
+          
+      var startPos;
 
       element.css({
        cursor: 'pointer'
       });
 
       element.on('mousedown', function(event) {
+        if (!$(event.target).hasClass('drag-handle')) return;
+        startPos = element.position();
+        startX = event.pageX;
+        startY = event.pageY;
         // Prevent default dragging of selected content
         event.preventDefault();
-        startX = event.pageX - x;
-        startY = event.pageY - y;
         $document.on('mousemove', mousemove);
         $document.on('mouseup', mouseup);
       });
 
       function mousemove(event) {
-        y = event.pageY - startY;
-        x = event.pageX - startX;
+        x = startPos.left + (event.pageX - startX);
+        y = startPos.top + (event.pageY - startY);
         element.css({
           top: y + 'px',
           left:  x + 'px'
