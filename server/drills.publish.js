9dd79757466407f3aae49e2fdc6a53303226d2ba
Matt Cameron <cameron.matt@gmail.com>
2017-10-31 17:21:58 -0500

    remove console messages in server

--- server/drills.publish.js
+++ server/drills.publish.js
@@ -10,24 +10,6 @@ Meteor.publish('drills', function(options, searchString) {
     // limit to users drills. TODO: add publicly shared drills?
     'userId': Meteor.userId()
   };
-  console.log(where);
   Counts.publish(this, 'numberOfDrills', Drills.find(where), {noReady: true});
   return Drills.find(where, options);
 });
-
-
-// Meteor.publish('drills', function(options, searchString) {
-//   var where = {
-//     'name': {
-//       '$regex': '.*' + (searchString || '') + '.*',
-//       '$options': 'i'
-//     },
-//     // limit to users drills. TODO: add publicly shared drills?
-//     //'owner': Meteor.userId()
-//   };
-
-// console.log(searchString, where);
-
-//   Counts.publish(this, 'numberOfDrills', Drills.find(where), {noReady: true});
-//   return Drills.find(where, options);
-// });
