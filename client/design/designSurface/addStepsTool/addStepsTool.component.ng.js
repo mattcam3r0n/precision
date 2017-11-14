1db13166fcb8756a1afe701d6df50bcd66eff35e
Matt Cameron <cameron.matt@gmail.com>
2017-11-14 11:16:10 -0600

    addStepsTool - wired up backspace button

--- client/design/designSurface/addStepsTool/addStepsTool.component.ng.js
+++ client/design/designSurface/addStepsTool/addStepsTool.component.ng.js
@@ -55,7 +55,7 @@ angular.module('drillApp')
       }
 
       $scope.backspaceDelete = function() {
-        
+        drillEditorService.deleteBackspace();
       }
 
       function activate(memberSelection) {
