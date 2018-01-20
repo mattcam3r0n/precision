ef21b1c59db245e2d729fb61512c5664aeb5ad0f
Matt Cameron <cameron.matt@gmail.com>
2018-01-20 12:58:32 -0600

    fixed playback bug when no music

--- client/lib/Drill/DrillScheduler.js
+++ client/lib/Drill/DrillScheduler.js
@@ -5,21 +5,25 @@ class DrillScheduler {
     }
 
     createSchedule(drill) {
-        var saveCount = drill.count;
+        let saveCount = drill.count;
 
-        var schedule = {
+        let schedule = {
             steps: [],
-            music: this.getDistinctFiles(drill)
+            music: this.getDistinctFiles(drill),
         };
 
         // TODO: need a way to get lenght of drill / end count
-        var player = new DrillPlayer(drill);
-        var lastTime = 0;
-        var lastTimeInterval = 0;
-        var startCount = drill.count + 1;
+        let player = new DrillPlayer(drill);
+        let lastTime = 0;
+        let lastTimeInterval = 0;
+        let startCount = drill.count + 1;
         while (!player.isEndOfDrill()) {
             player.stepForward();
-            var step = this.createCountDescriptor(drill, startCount, drill.count, lastTime, lastTimeInterval);
+            let step = this.createCountDescriptor(drill,
+                startCount,
+                drill.count,
+                lastTime,
+                lastTimeInterval);
             lastTime = step.time;
             lastTimeInterval = step.timeInterval;
             schedule.steps.push(step);
@@ -33,59 +37,68 @@ class DrillScheduler {
     }
 
     getDistinctFiles(drill) {
-        return [...new Set(drill.music.map(m => m.url))]
+        if (!drill.music) {
+            return [];
+        }
+        return [...new Set(drill.music.map((m) => m.url))];
     }
 
     getMusicAtCount(drill, count) {
         if (!drill.music) return null;
 
-        return drill.music.find(m => {
+        return drill.music.find((m) => {
             return m.startCount <= count && m.endCount >= count;
         });
     }
 
-    createCountDescriptor(drill, startCount, count, lastTime, lastTimeInterval) {
-        var music = this.getMusicAtCount(drill, count);
-        var tempo = music ? music.tempo : drill.tempo || 120;
-        var timeInterval = 60 / tempo; 
+    createCountDescriptor(drill, startCount, count,
+                            lastTime, lastTimeInterval) {
+        let music = this.getMusicAtCount(drill, count);
+        let tempo = music ? music.tempo : drill.tempo || 120;
+        let timeInterval = 60 / tempo;
         if (music && music.beats && music.beats[count - startCount]) {
             timeInterval = music.beats[count - music.startCount].timeInterval;
         }
         // if timeInterval is 0 (at start of new music), use lastTimeInterval to add
         // appropriate space between stop of last clip and start of new
-        var time = count == startCount ? 0 : lastTime + (timeInterval || lastTimeInterval);
+        let time = count == startCount
+            ? 0
+            : lastTime + (timeInterval || lastTimeInterval);
 
         return {
             count: count,
             time: time,
             timeInterval: timeInterval,
             tempo: tempo,
-            music: this.createMusicDescriptor(music, startCount, count, timeInterval)
+            music: this.createMusicDescriptor(music, startCount,
+                                                count, timeInterval),
         };
     }
 
     createMusicDescriptor(music, startCount, count, tempoInterval) {
         if (!music) return null;
 
-        var offset = 0;
+        let offset = 0;
         if (music.startCount < count) {
             if (music && music.beats && music.beats.length > 0) {
                 offset = music.beats[count - music.startCount].timeOffset;
             } else {
-                offset = (count - music.startCount) * tempoInterval;                
+                offset = (count - music.startCount) * tempoInterval;
             }
         }
 
-        var startOffset = music.startOffset + offset;
-        var duration = music.duration - offset;
-        
+        let startOffset = music.startOffset + offset;
+        let duration = music.duration - offset;
+
         return {
             fileName: music.fileName,
             url: music.url,
-            startCount: startCount > music.startCount ? startCount : music.startCount,
+            startCount: startCount > music.startCount
+                ? startCount
+                : music.startCount,
             endCount: music.endCount,
             startOffset: startOffset,
-            duration: duration
+            duration: duration,
         };
     }
 }
