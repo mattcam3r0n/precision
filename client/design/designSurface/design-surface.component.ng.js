6860ddd91a5fb304457cf12772e4054447fc915f
Matt Cameron <cameron.matt@gmail.com>
2018-02-03 13:21:13 -0600

    only resize when on design route

--- client/design/designSurface/design-surface.component.ng.js
+++ client/design/designSurface/design-surface.component.ng.js
@@ -8,22 +8,23 @@ angular.module('drillApp')
   .component('designSurface', {
     templateUrl: 'client/design/designSurface/design-surface.view.ng.html',
     bindings: {
-      drill: '<'
+      drill: '<',
     },
-    controller: function ($scope, $window, $timeout, appStateService, drillEditorService, eventService) {
-      var ctrl = this;
+    controller: function($scope, $window, $timeout, $location,
+        appStateService, drillEditorService, eventService) {
+      let ctrl = this;
 
       ctrl.$onInit = function() {
         ctrl.subscriptions = new EventSubscriptionManager(eventService);
-
         eventService.notify(Events.showSpinner);
-        $timeout(function(){
+        $timeout(function() {
           ctrl.field = new FieldController(ctrl.drill, eventService);
           appStateService.field = ctrl.field;
           eventService.notify(Events.hideSpinner);
         });
-  
-        angular.element($window).bind('resize', function () {
+
+        angular.element($window).bind('resize', function() {
+          if ($location.path() != '/') return; // only resize if we're on field page
           ctrl.field.resize();
         });
 
@@ -31,54 +32,52 @@ angular.module('drillApp')
           if (!ctrl.field) return;
           ctrl.field.drillStateChanged(args);
         });
-  
+
         ctrl.subscriptions.subscribe(Events.membersAdded, () => {
-          ctrl.field.membersChanged();        
+          ctrl.field.membersChanged();
         });
-  
+
         ctrl.subscriptions.subscribe(Events.showPaths, (evt, args) => {
-          ctrl.field.showPaths();        
+          ctrl.field.showPaths();
         });
-  
+
         ctrl.subscriptions.subscribe(Events.strideTypeChanged, (evt, args) => {
           ctrl.field.strideTypeChanged(args.strideType);
         });
-  
+
         ctrl.subscriptions.subscribe(Events.resize, () => {
           ctrl.field.resize();
         });
-  
+
         ctrl.subscriptions.subscribe(Events.sizeToFit, () => {
           ctrl.field.sizeToFit();
         });
-  
+
         ctrl.subscriptions.subscribe(Events.zoomIn, () => {
           ctrl.field.zoomIn();
         });
-  
+
         ctrl.subscriptions.subscribe(Events.zoomOut, () => {
           ctrl.field.zoomOut();
         });
-  
+
         ctrl.subscriptions.subscribe(Events.updateField, (evt, args) => {
           ctrl.field.update();
         });
-
-      }
+      };
 
       ctrl.$onDestroy = function() {
         ctrl.field.canvas.dispose();
         appStateService.field = null;
         ctrl.subscriptions.unsubscribeAll();
-      }
+      };
 
       ctrl.$onChanges = function(changes) {
         // if the drill changed, update field
         if (!ctrl.field) return;
         ctrl.field.setDrill(ctrl.drill);
-      }
-      
-    }
+      };
+    },
   });
 
 
