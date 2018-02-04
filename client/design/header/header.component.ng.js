34b29cc314ff86f56834a66307df77a08a147274
Matt Cameron <cameron.matt@gmail.com>
2018-02-04 10:38:18 -0600

    separated design header. submit login form on enter.

---
+++ client/design/header/header.component.ng.js
@@ -0,0 +1,63 @@
+'use strict';
+
+import Events from '/client/lib/Events';
+
+angular.module('drillApp')
+  .component('designHeader', {
+    templateUrl: 'client/design/header/header.component.ng.html',
+    bindings: {
+    },
+    controller: function($scope, eventService,
+              appStateService, drillEditorService) {
+      let ctrl = this;
+
+      ctrl.$onInit = function() {
+        // $('[data-toggle="tooltip"]').tooltip();
+        ctrl.subscriptions = eventService.subscribe(Events.drillOpened,
+              onDrillOpened);
+      };
+
+      ctrl.$onChanges = function(changes) {
+        $scope.drillName = drillEditorService.drill.name;
+      };
+
+      ctrl.$onDestroy = function() {
+
+      };
+
+      ctrl.onNewDrill = function() {
+        eventService.notify(Events.newDrill);
+      };
+
+      ctrl.onOpenDrill = function() {
+        eventService.notify(Events.showOpenDrillDialog);
+      };
+
+      ctrl.onDrillProperties = function() {
+        eventService.notify(Events.showDrillPropertiesDialog);
+      };
+
+      ctrl.onDebug = function() {
+        console.log(drillEditorService.drill);
+      };
+
+      ctrl.focusDrillName = function() {
+        angular.element('#txtDrillName').focus();
+      };
+
+      ctrl.drillName = function() {
+        if (!drillEditorService.drill) return;
+
+        return drillEditorService.drill.name;
+      };
+
+      $scope.onNameChange = function() {
+        appStateService.drill.name = $scope.drillName;
+        drillEditorService.save(true);
+      };
+
+      function onDrillOpened() {
+        $scope.drillName = appStateService.drill.name;
+      }
+    },
+  });
