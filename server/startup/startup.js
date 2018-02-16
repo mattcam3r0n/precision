d33c83b3b07b9adae0996b8db821b17eb7e03443
Matt Cameron <cameron.matt@gmail.com>
2018-02-15 22:00:06 -0600

    default nammborg email to admin

--- server/startup/startup.js
+++ server/startup/startup.js
@@ -2,5 +2,12 @@ import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(function() {
     const matt = Accounts.findUserByEmail('cameron.matt@gmail.com');
-    Roles.addUsersToRoles( matt._id, ['admin']);
+    if (matt) {
+        Roles.addUsersToRoles( matt._id, ['admin']);
+    }
+
+    const nammb = Accounts.findUserByEmail('nammb.org@gmail.com');
+    if (nammb) {
+        Roles.addUsersToRoles( nammb._id, ['admin']);
+    }
 });
