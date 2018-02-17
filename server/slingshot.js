152123d8f09b4c4611693e9455aaeaaaa17f4bf7
Matt Cameron <cameron.matt@gmail.com>
2018-02-16 21:38:20 -0600

    allow m4a upload

--- server/slingshot.js
+++ server/slingshot.js
@@ -1,20 +1,22 @@
 import FileUploader from '/lib/FileUploader';
 
-Slingshot.fileRestrictions("uploadToAmazonS3", {
-    allowedFileTypes: [ "audio/midi", 
-        "audio/mpeg",
-        "audio/mp3", 
-        "audio/webm", 
-        "audio/ogg", 
-        "audio/wav" ],
-    maxSize: 10 * 1024 * 1024
+Slingshot.fileRestrictions('uploadToAmazonS3', {
+    allowedFileTypes: ['audio/midi',
+        'audio/mpeg',
+        'audio/mp3',
+        'audio/webm',
+        'audio/ogg',
+        'audio/wav',
+        'audio/m4a',
+     ],
+    maxSize: 10 * 1024 * 1024,
 });
 
-Slingshot.createDirective("uploadToAmazonS3", Slingshot.S3Storage, {
-    region: "us-east-2",
-    bucket: "precision-audio",
-    acl: "public-read",
-    authorize: function () {
+Slingshot.createDirective('uploadToAmazonS3', Slingshot.S3Storage, {
+    region: 'us-east-2',
+    bucket: 'precision-audio',
+    acl: 'public-read',
+    authorize: function() {
         // let userFileCount = Files.find({ "userId": this.userId }).count();
         // return userFileCount < 3 ? true : false;
         // TODO: logic to authorize file upload goes here...
@@ -22,10 +24,10 @@ Slingshot.createDirective("uploadToAmazonS3", Slingshot.S3Storage, {
         //   * has upload permissions?
         return Meteor.userId();
     },
-    key: function (file) {
+    key: function(file) {
         // var user = Meteor.users.findOne(this.userId);
         // return user.emails[0].address + "/" + file.name;
         // TODO: logic to generate unique key (file name) goes here
         return FileUploader.key(file);
-    }
+    },
 });
