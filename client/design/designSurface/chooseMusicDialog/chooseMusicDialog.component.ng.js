4b960ae3a66200f350dad10a7e1c66a679e156bb
Matt Cameron <cameron.matt@gmail.com>
2018-03-05 14:28:24 -0600

    fixed pagination problem

--- client/design/designSurface/chooseMusicDialog/chooseMusicDialog.component.ng.js
+++ client/design/designSurface/chooseMusicDialog/chooseMusicDialog.component.ng.js
@@ -78,6 +78,7 @@ angular.module('drillApp')
       };
 
       $scope.pageChanged = function(newPage) {
+        console.log('chooseMusic pageChanged', $scope.page, newPage);
         $scope.page = newPage;
       };
 
