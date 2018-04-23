2cdf53601e30ab844658479fa9dc1bc443fb51ff
Matt Cameron <cameron.matt@gmail.com>
2018-04-22 20:44:03 -0500

    hide delete on current drill in open dialog

--- client/design/openDrillDialog/openDrillDialog.component.ng.js
+++ client/design/openDrillDialog/openDrillDialog.component.ng.js
@@ -2,99 +2,124 @@
 
 import Events from '/client/lib/Events';
 
-angular.module('drillApp')
-  .component('openDrillDialog', {
-    templateUrl: 'client/design/openDrillDialog/openDrillDialog.view.ng.html',
-    bindings: {
-      onOpen: '&',
-    },
-    controller: function($scope, appStateService,
-        eventService, confirmationDialogService) {
-      let ctrl = this;
+angular.module('drillApp').component('openDrillDialog', {
+  templateUrl: 'client/design/openDrillDialog/openDrillDialog.view.ng.html',
+  bindings: {
+    onOpen: '&',
+  },
+  controller: function(
+    $scope,
+    appStateService,
+    eventService,
+    confirmationDialogService
+  ) {
+    let ctrl = this;
 
-      $scope.searchOptions = { searchText: '', searchMyDrills: true, searchSharedDrills: false };
-      $scope.page = 1;
-      $scope.perPage = 5;
-      $scope.sort = {}; // { name_sort: 1 };
-      $scope.orderProperty = '1';
+    $scope.searchOptions = {
+      searchText: '',
+      searchMyDrills: true,
+      searchSharedDrills: false,
+    };
+    $scope.page = 1;
+    $scope.perPage = 5;
+    $scope.sort = {}; // { name_sort: 1 };
+    $scope.orderProperty = '1';
 
-      $scope.subscribe('drills', function() {
-        return [{
+    $scope.subscribe('drills', function() {
+      return [
+        {
           sort: $scope.getReactively('sort'),
           limit: parseInt($scope.getReactively('perPage')),
-          skip: ((parseInt($scope.getReactively('page'))) - 1)
-            * (parseInt($scope.getReactively('perPage'))),
+          skip:
+            (parseInt($scope.getReactively('page')) - 1) *
+            parseInt($scope.getReactively('perPage')),
         },
-          $scope.getReactively('searchOptions.searchText'),
-          $scope.getReactively('searchOptions.searchMyDrills'),
-          $scope.getReactively('searchOptions.searchSharedDrills'),
-        ];
-      });
+        $scope.getReactively('searchOptions.searchText'),
+        $scope.getReactively('searchOptions.searchMyDrills'),
+        $scope.getReactively('searchOptions.searchSharedDrills'),
+      ];
+    });
 
-      $scope.helpers({
-        drillCount: function() {
-          return Counts.get('numberOfDrills');
-        },
-        drills: function() {
-          return Drills.find({}, {
+    $scope.helpers({
+      drillCount: function() {
+        return Counts.get('numberOfDrills');
+      },
+      drills: function() {
+        return Drills.find(
+          {},
+          {
             sort: $scope.getReactively('sort'),
-          });
-        },
+          }
+        );
+      },
+    });
+
+    $scope.open = function(selectedDrill) {
+      eventService.notify(Events.showSpinner);
+      appStateService.openDrill(selectedDrill._id).then((drill) => {
+        eventService.notify(Events.hideSpinner);
       });
+    };
 
-      $scope.open = function(selectedDrill) {
-        eventService.notify(Events.showSpinner);
-        appStateService.openDrill(selectedDrill._id).then((drill) => {
-          eventService.notify(Events.hideSpinner);
-        });
-      };
-
-      $scope.delete = function(drill) {
-        confirmationDialogService.show({
+    $scope.delete = function(drill) {
+      confirmationDialogService
+        .show({
           heading: 'Delete Drill',
           message: 'Are you sure you want delete "' + drill.name + '"?',
           confirmText: 'Delete',
-        }).then((result) => {
+        })
+        .then((result) => {
           if (result.confirmed) {
             appStateService.deleteDrill(drill._id);
           }
         });
-      };
+    };
 
-      $scope.canDelete = function(drill) {
-        return $scope.currentUser
-          && drill
-          && (drill.userId === $scope.currentUser._id);
-      };
+    $scope.isCurrentDrill = function(drill) {
+      const currentDrillId = appStateService.drill
+        ? appStateService.drill._id
+        : null;
+      return drill._id == currentDrillId;
+    };
 
-      $scope.pageChanged = function(newPage) {
-        $scope.page = newPage;
-      };
+    $scope.canDelete = function(drill) {
+      return (
+        $scope.currentUser &&
+        drill &&
+        drill.userId === $scope.currentUser._id &&
+        !$scope.isCurrentDrill(drill)
+      );
+    };
 
-      const unwatchOrderProperty = $scope.$watch('orderProperty', function() {
-        if ($scope.orderProperty) {
-          $scope.sort = {
-            name_sort: parseInt($scope.orderProperty),
-          };
+    $scope.pageChanged = function(newPage) {
+      $scope.page = newPage;
+    };
+
+    const unwatchOrderProperty = $scope.$watch('orderProperty', function() {
+      if ($scope.orderProperty) {
+        $scope.sort = {
+          name_sort: parseInt($scope.orderProperty),
+        };
+      }
+    });
+
+    const unwatchSharedDrills = $scope.$watch(
+      'searchOptions.searchSharedDrills',
+      function(newValue, oldValue) {
+        if (
+          !$scope.searchOptions.searchSharedDrills &&
+          !$scope.searchOptions.searchMyDrills
+        ) {
+          $scope.searchOptions.searchMyDrills = true;
         }
-      });
+      }
+    );
 
-      const unwatchSharedDrills = $scope.$watch('searchOptions.searchSharedDrills',
-        function(newValue, oldValue) {
-          if (!$scope.searchOptions.searchSharedDrills
-              && !$scope.searchOptions.searchMyDrills) {
-            $scope.searchOptions.searchMyDrills = true;
-          }
-      });
+    ctrl.$onInit = function() {};
 
-      ctrl.$onInit = function() {
-      };
-
-      ctrl.$onDestroy = function() {
-        unwatchOrderProperty();
-        unwatchSharedDrills();
-      };
-    },
-  });
-
-
+    ctrl.$onDestroy = function() {
+      unwatchOrderProperty();
+      unwatchSharedDrills();
+    };
+  },
+});
